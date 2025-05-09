f9feaf70eb1d43603d70fe4a332b629312d4c50e
naokicode <kitajimanaoki.0917@icloud.com>
2025-05-09 14:39:58 +0900

    エラーシリ

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import { CopyrightYear } from "@/components/copyright-year";
 
 export function Footer() {
   return (
-    <footer className="border-t py-6 md:py-10">
+    <footer className="border-t py-6 md:py-10 mx-auto">
       <div className="container flex flex-col items-center justify-between gap-4 md:flex-row">
         <p className="text-center text-sm text-muted-foreground md:text-left">
           <CopyrightYear />
